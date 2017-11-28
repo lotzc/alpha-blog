ad0c867783bc0367ff597460602de9c2c623c08a
Chris Lotz <chrisplotz@gmail.com>
2017-11-28 09:30:16 -0800

    add article and category association

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -23,6 +25,13 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  def require_admin
+    if !logged_in? || (logged_in and !current_user.admin?)
+      flash[:danger] = "Only Admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 
   def category_params
     params.require(:category).permit(:name)
